df5f8a9229cd8fe3d9256a654730023b5961b860
Gluz <brunogluszczukjobs@gmail.com>
2023-02-18 17:20:49 -0300

    refactor: delete unused directory

--- strategy/test_tax_calculator.py
+++ strategy/test_tax_calculator.py
@@ -1,6 +1,7 @@
 from strategy.functional_approach.tax import calculates_income_tax, calculate_xpto_tax, IncomeTax, XptoTax
 from strategy.functional_approach.tax_calculator import TaxCalculator
 
+
 # Functional approach
 def test_should_calculate_income_tax_with_correctly_percentage():
     tax_calculator = TaxCalculator()
@@ -21,6 +22,7 @@ def test_should_calculate_income_tax_with_correctly_percentage():
     tax_calculator = TaxCalculator()
     income_tax = tax_calculator.calculate_tax(budget=500, tax=IncomeTax().calculate)
     assert income_tax == 50.0
+
 
 def test_should_calculate_xpto_tax_with_correctly_percentage():
     tax_calculator = TaxCalculator()
